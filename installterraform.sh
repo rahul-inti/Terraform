504280a73c10e47c2831d9ff32ae0d61c2e9b266
Inti Rahul - (Digital) <in20071139@wipro.com>
2020-03-15 18:04:20 +0530

    jnk

--- installterraform.sh
+++ installterraform.sh
@@ -46,7 +46,6 @@ else
    echo ""
    echo "Extracting Terraform executable"
    unzip terraform_0.12.23_linux_amd64.zip
-   mv terraform_0.12.23_linux_amd64.zip terraform
    sudo mv terraform /usr/local/bin/
 
 #  rm "$DOWNLOADED_FILE"
